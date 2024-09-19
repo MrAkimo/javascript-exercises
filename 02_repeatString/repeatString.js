b657aebfccef48550c37adfde3cb8368914909b6
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-19 21:21:12 +0200

    completed excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringText, amountRepeat) {
+    let repeatedString = '';
+    if (amountRepeat < 0){
+        repeatedString = 'ERROR';
+        return repeatedString;
+    }
+    for (let i = 0; i < amountRepeat; i++) {
+        repeatedString += stringText;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
