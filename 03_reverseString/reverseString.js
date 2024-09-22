b65c348998dc4367fd998b02c02a9a1b17978fbb
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-22 21:15:12 +0200

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(textS) {
+    let reverseText = ''
+    for(let i = textS.length - 1; i >= 0; i--) {
+        reverseText += textS[i];
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
