6133e59ebfd262cee20bb2662ec6fcb88ddedf68
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-24 21:11:25 +0200

    excercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayData, ...toRemove) {
+
+    for (let x = 0; x < toRemove.length; x++){
+        for(let i = 0; i < arrayData.length; i++){
+            if (arrayData[i] === toRemove[x]) {
+                arrayData.splice(i,1);
+                i -=1
+            }
+        }
+    }
+    return arrayData
 };
 
 // Do not edit below this line
