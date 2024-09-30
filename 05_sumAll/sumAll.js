fcaed576db4a1c84cecc9f23120f3a1501704dd3
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-09-30 12:59:42 +0200

    exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(initialValue, endValue) {
+    let sumTotal = 0;
+    if( Number.isInteger(initialValue) && Number.isInteger(endValue)) {
+        if(initialValue > 0 && endValue > 0){
+            if(initialValue > endValue) {
+                for(let i = endValue; i <= initialValue; i++) {
+                    sumTotal += i;
+                }
+                return sumTotal;
+            } else {
+                for(let i = initialValue; i <= endValue; i++) {
+                    sumTotal += i;
+                }
+                return sumTotal;
+            }
+        } else return 'ERROR';
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
