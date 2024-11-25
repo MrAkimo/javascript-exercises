a955aa966f6a234a6782521540bc2870d4f7269d
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-11-25 19:59:03 +0100

    finished excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alfaNum = '0123456789abcdefghijklmnopqrstuvwxyz'
 
+    let cleanedArr = word
+    .toLowerCase()
+    .split('')
+    .filter(word => alfaNum.includes(word))
+    .join('');
+    
+    let reverseArr = cleanedArr.split('').reverse().join('');
+
+    return cleanedArr === reverseArr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
