7352ec721a57a628f240abf9604ff0d6d6bc316d
MrAkimo <73078767+MrAkimo@users.noreply.github.com>
2024-11-26 21:00:38 +0100

    excercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function( pos) {
+    if(pos == 0) return 0
+    if(pos < 0) return 'OOPS'
+    let arrayFibo = [1,1];
+    for (let index = 2; index <= pos; index++) {
+        arrayFibo.push(arrayFibo[index - 1] + arrayFibo[index - 2])  
+    }
+    
+    console.table(arrayFibo);
+    console.log(arrayFibo[pos])
+    return arrayFibo[pos - 1];
 };
+
+fibonacci(4)
 
 // Do not edit below this line
 module.exports = fibonacci;
